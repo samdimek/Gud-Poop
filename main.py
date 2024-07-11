6abcadd7d72768aef3347dd0fc07875e2d243bfa
samdimek <delashoo.uk@gmail.com>
2024-07-11 06:27:18 +0300

    Chronological updates
    Added; cookieclicker.py, updated main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium.webdriver.support import expected_conditions as EC 
 import time
 
 # https://sites.google.com/chromium.org/driver/
@@ -14,12 +14,24 @@ import time
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://studybay.com/login/")
+driver.get("https://google.com")
 
-input_element = driver.find_element(By.CLASS_NAME, "input__Field-sc-hqbtr5-1 kbzBxw")
-input_element.clear()
-input_element.send_keys("billyodhiambo888@gmail.com" + Keys.ENTER)
+WebDriverWait(driver, 7).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
 
+# Brightdata is a company that solves the captcha issues
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")  # Finds the element on the website by its classname. 
+input_element.clear() # Clears the input spaces to make room for more inputs.
+input_element.send_keys("Studybay.com" + Keys.ENTER) # Sends inputs to the browser and presses Enter to search
+
+WebDriverWait(driver, 7).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Studybay.com"))
+)
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Studybay.com")
+link.click()
 
 time.sleep(10)
 
